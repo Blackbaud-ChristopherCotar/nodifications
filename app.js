0d6c3d0e135718bb2e10bf5d5602feeb98776761
Blackbaud-ChristopherCotar <christopher.cotar@blackbaud.com>
2014-02-09 20:07:09 -0600

    messing around

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-var app = require('express')()
+var express = require('express')
+  , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
 server.listen(3000);
 
+app.configure(function(){
+	app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function(req, res){
-  res.sendfile(__dirname + 'static/index.html');
+  res.sendfile(__dirname + '/static/index.html');
 });
 
 io.sockets.on('connection', function(socket){
